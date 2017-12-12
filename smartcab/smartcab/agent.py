6303afb7d4222607dffab8e19df4dac616426e7c
Ramirez, John (jr870d) <jr870d@us.att.com>
2017-12-11 18:37:00 -0600

    answered q4 and q5, modified agent.py

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -62,7 +62,7 @@ class LearningAgent(Agent):
         # With the hand-engineered features, this learning process gets entirely negated.
         
         # Set 'state' as a tuple of relevant data for the agent        
-        state = None
+        state = [waypoint, inputs, deadline]
 
         return state
 
@@ -169,7 +169,7 @@ def run():
     # Follow the driving agent
     # Flags:
     #   enforce_deadline - set to True to enforce a deadline metric
-    env.set_primary_agent(agent, True)
+    env.set_primary_agent(agent)
 
     ##############
     # Create the simulation
@@ -178,7 +178,7 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.01, log_metrics=True, )
+    sim = Simulator(env)
     
     ##############
     # Run the simulator
